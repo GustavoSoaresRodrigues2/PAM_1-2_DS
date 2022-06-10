74f50c01e4b7645f80b78a34fe8ceddb7f63822c
unknown <you@example.com>
2022-06-10 16:40:47 -0300

    alterando sagg

--- App.js
+++ App.js
@@ -11,28 +11,28 @@ export default function App() {
     <View>
       <Cabecalho />
       <Botao
-        logo="logo-google-playstore"
-        texto="App"
-        cor="#2ecc71"
+        logo="home"
+        texto="Pagina Inicial"
+        cor="#A9A9A9"
         logo2="list-outline"
-        texto2="Categorias"
-        cor2="#8e44ad"
+        texto2="Configurações"
+        cor2="#A9A9A9"
       />
       <Botao
         logo="camera-outline"
-        texto="Filmes"
-        cor="#95a5a6"
+        texto="Serviços Online"
+        cor="#FF6347"
         logo2="book-outline"
-        texto2="Livros"
-        cor2="#1abc9c"
+        texto2="Isenções"
+        cor2="#FF6347"
       />
       <Botao
         logo="musical-notes-outline"
-        texto="Músicas"
-        cor="#e67e22"
+        texto="Consultas Grátis"
+        cor="#1C1C1C"
         logo2="game-controller"
-        texto2="Jogos"
-        cor2="#f1c40f"
+        texto2="Fale Conosco"
+        cor2="#1C1C1C"
       />
 
       <Text style = {{fontSize: 20, margin: 10, marginTop: 20}}>
